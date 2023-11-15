5e0ea92d34eb8d75b78ba7228c83a6426de7435d
cooltonie <nwachukwua99@gmail.com>
2023-11-15 21:30:35 +0100

    include styles and extra function to app

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -7,9 +7,9 @@ export const Todo = ({task, toggleComplete, deleteTodo, editTodo}) => {
   return (
     <div className='Todo'>
         <p onClick={() => toggleComplete(task.editTodo)} className={`${task.completed ? 'completed': ""}`}>{task.task}</p>
-        <div>
-            <FontAwesomeIcon icon={faPenToSquare} onClick={() => editTodo(task.id)}/>
-            <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo (task.id)}/>
+        <div className='butts'>
+            <FontAwesomeIcon className='edit' icon={faPenToSquare} onClick={() => editTodo(task.id)}/>
+            <FontAwesomeIcon className='delete' icon={faTrash} onClick={() => deleteTodo (task.id)}/>
         </div>
     </div>
   )
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -40,7 +40,7 @@ export const TodoWrapper = () => {
             )
         ))}
 
-
+   
     </div>
     
   )
